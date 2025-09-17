b9b62810e003da16605a5d5b4010e93c764da3b0
jeesgit <josuasiregar47@gmail.com>
2025-09-17 10:44:45 +0900

    menambah folder latihan-js-dasar

---
+++ latihan-js-dasar/komentar.js
@@ -0,0 +1,10 @@
+//Komentar satu baris
+
+/* 
+    Komentar beberapa baris
+    Menjelaskan bagian program
+
+*/
+
+let nama = "Lavender"; //Menyimpan nama siswa
+console.log("Nama saya", nama); //Tampilkan ke layar
---
+++ latihan-js-dasar/tipe-data.js
@@ -0,0 +1,20 @@
+//Tipe data 
+console.log("=== "+"Tipe Data"+" ===");
+console.log(" ");
+let nama = "Daisy";
+let umur = 25;
+let lulus = true;
+let nilai = null;
+let alamat;
+let hobi = ["Baca", "Nyanyi"];
+let siswa = {nama: "Roses", kelas: "Web"};
+
+console.log("Nama:", nama);
+console.log("Umur:", umur);
+console.log("Lulus:", lulus);
+console.log("Nilai:", nilai);
+console.log("Alamat:", alamat);
+console.log("Hobi:", hobi);
+console.log("Suiswa:", siswa);
+
+
