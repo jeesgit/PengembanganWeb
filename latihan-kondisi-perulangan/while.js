ec5cd2cd39b5a74a59c0a498b21a5a42a435ce61
jeesgit <josuasiregar47@gmail.com>
2025-09-17 15:33:25 +0900

    menambah folder kondisional

---
+++ latihan-kondisi-perulangan/while.js
@@ -0,0 +1,16 @@
+let ulang =  true;
+
+do{
+let i = 1;
+
+console.log("Menampilkan bilangan genap dari 1 sampai 20: ");
+while (i <= 20){
+    if( i%2 === 0){
+        console.log(i);
+    }
+    i++;
+}
+
+//ubah ulkang jadi false agar tidak mengulang lagi
+ulang = false;
+}while(ulang);
