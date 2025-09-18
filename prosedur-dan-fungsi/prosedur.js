d252c5e434fa076c9ecd1a3039a374bd6685673c
jeesgit <josuasiregar47@gmail.com>
2025-09-18 15:54:49 +0900

    menambah folder array

---
+++ prosedur-dan-fungsi/prosedur.js
@@ -0,0 +1,28 @@
+//Prosedur
+console.log("===Prosedur===");
+console.log(" ");
+
+function halo(){
+    console.log("Selamat pagi...!");
+}
+halo();
+
+function salam(siapa){
+    console.log(`Selamat pagi ${siapa}...!`);
+}
+salam("semua");
+console.log(" ");
+
+//Fungsi
+console.log("===Fungsi===");
+console.log(" ");
+
+function sapa(){
+    return "Selamat pagi...!";
+}
+console.log(sapa());
+
+function pagi(siapa){
+    return `Selamat pagi ${siapa}...!`;
+}
+console.log(pagi("semua"));
