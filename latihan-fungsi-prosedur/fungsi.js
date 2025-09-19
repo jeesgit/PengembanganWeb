6e3e3a1f73bd2d3871af5b2fc2d4fc5067b99ad8
jeesgit <josuasiregar47@gmail.com>
2025-09-19 15:25:14 +0900

    menambah folder akses-file

---
+++ latihan-fungsi-prosedur/fungsi.js
@@ -0,0 +1,22 @@
+function hitungLuas(sisi){
+    return sisi*sisi;
+}
+
+//Fungsi untuk menghitung keliling persegi
+function hitungKeliling(sisi){
+    return 4 * sisi;
+}
+
+//Fungsi utama untuk menampilkan hasil
+function tampilkanPersegi(sisi){
+    console.log("============ Persegi ============");
+    console.log("Sisi               :", sisi);
+    console.log("Luas Persegi       :", hitungLuas(sisi));
+    console.log("Keliling Persegi   :", hitungKeliling(sisi));
+    console.log("=================================");
+
+}
+
+//Pemanggilan fungsi
+tampilkanPersegi(7);
+tampilkanPersegi(23);
---
+++ latihan-kompilasi/kompilasi.js
@@ -0,0 +1,16 @@
+let input = "lima";  //ubah menjadi 5 agar berhasil
+
+
+try{
+    let angka =  parseInt(input);
+
+    if(isNaN(angka)){
+        throw new Error("Input harus berupa angka!");
+    }
+
+    console.log("Kuadrat:", angka * angka);
+}
+catch(e){
+    console.error("Terjadi kesalahan:", e.message);
+}
+
