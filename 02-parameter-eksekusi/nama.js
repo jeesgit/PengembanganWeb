354a2752e188b823d27263ee5c238bd0dce656c6
jeesgit <112079728+jeesgit@users.noreply.github.com>
2025-09-25 08:09:19 +0900

    menambah folder parameter-eksekusi

---
+++ 02-parameter-eksekusi/nama.js
@@ -0,0 +1,5 @@
+//mengambil nama dari perintah eksekusi yaang dimasukkan pengguna
+const nama = process.argv[2];
+
+//menampilkan greeting sesuai nama pengguna
+console.log("Selamat datang, ", nama);
