d3cc03b23b2987358b7703ba1aba8a2ab4ecce74
jeesgit <josuasiregar47@gmail.com>
2025-09-25 10:00:01 +0900

    update folder

--- 02-parameter-eksekusi/nama.js
+++ 02-parameter-eksekusi/nama.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 //mengambil nama dari perintah eksekusi yaang dimasukkan pengguna
 const nama = process.argv[2];
 
 //menampilkan greeting sesuai nama pengguna
-=======
-//mengambil nama dari perintah eksekusi yaang dimasukkan pengguna
-const nama = process.argv[2];
-
-//menampilkan greeting sesuai nama pengguna
->>>>>>> b1d3da267244640b017b4a299a59df354335aadf
 console.log("Selamat datang, ", nama);
