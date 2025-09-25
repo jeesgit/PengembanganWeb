85ab5456a6b4fd89fe2aee7247bc5712bf645b81
jeesgit <josuasiregar47@gmail.com>
2025-09-25 11:31:02 +0900

    update folder eksekusi-program

--- eksekusi-program/hitungKata.js
+++ eksekusi-program/hitungKata.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const readline = require('readline');
 
 //buat interface untuk baca input terminal
@@ -15,25 +14,3 @@ rl.question('Masukkan kalimat: ', (input)=>{
     console.log(`- Jumlah huruf     :   ${jumlahHuruf}`);
     rl.close();
 });
-
-
-=======
-const readline = require('readline');
-
-//buat interface untuk baca input terminal
-const rl = readline.createInterface({input: process.stdin,
-    output: process.stdout
-});
-
-//minta user memasukkan kalimat
-rl.question('Masukkan kalimat: ', (input)=>{
-    const jumlahKata = input.trim().split(/\s+/).length;
-    const jumlahHuruf = input.replace(/\s+/g, '').length;
-    console.log(`\n Hasil Analisis  :`);
-    console.log(`- Jumlah kata      :   ${jumlahKata}`);
-    console.log(`- Jumlah huruf     :   ${jumlahHuruf}`);
-    rl.close();
-});
-
-
->>>>>>> b1d3da267244640b017b4a299a59df354335aadf
